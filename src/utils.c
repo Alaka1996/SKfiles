a1975635bfc962c72d2735f55c39f58e071fddea
Alaka1996 <bgalakananda@gmail.com>
2025-01-05 16:00:24 +0530

    Update utils.c

--- src/utils.c
+++ src/utils.c
@@ -2,10 +2,7 @@
 #include "utils.h"  // Assuming you have a header file for utils
 
 int calculate_average(const uint16_t *data, int size) {
-    if (size <= 0) {
-        return -1; // Error: Invalid size
-    }
-
+    
     int sum = 0;
     for (int i = 0; i < size; i++) {
         sum += data[i];
